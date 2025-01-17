1f6027edb4e5cc22ba760702c0ee56189465966d
AjayM2004 <ajay97908940@gmail.com>
2025-01-17 13:25:39 +0530

    added Sort0s1s2s.js

--- Arrays/Sort0s1s2s.js
+++ Arrays/Sort0s1s2s.js
@@ -1,1 +1,28 @@
 
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+  //dutch national flag algorithm
+  let low = 0;
+  let high = nums.length-1;
+  let mid = 0;
+  while(mid<=high)
+  {
+      if(nums[mid]==0)
+      {
+          [nums[mid],nums[low]] = [nums[low],nums[mid]];
+          low++;
+          mid++;
+      }
+      else if(nums[mid]==1)
+      {
+          mid++;
+      }
+      else{
+          [nums[mid],nums[high]] = [nums[high],nums[mid]];
+          high--;
+      }
+  }
+};
